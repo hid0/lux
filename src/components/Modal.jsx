5f185d96d1af0918d6f6a96f9a3681cf3de975ce
Faiz Hidayatulloh <faizhid11@gmail.com>
2022-04-02 16:20:37 +0700

    set project to PWA on service worker

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ export default function Modal({ handlerShowModal }) {
     <>
       <div className="fixed inset-0 z-40 flex items-center justify-center w-100 min-h-screen" onClick={handlerShowModal}>
         <div className="fixed inset-0 bg-black opacity-35"></div>
-        <div className="bg-white p-0 md:w-88 md:pb-2 relative z-50">
+        <div className="bg-white p-0 md:w-88 md:pb-6 z-10 rounded shadow">
           <div className="w-screen pb-56 md:w-88 md:pb-56 relative z-50">
             <div className="absolute w-full h-full">
               <iframe
