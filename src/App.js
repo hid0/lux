9d478537f7efbd9930e1e2fc782af2db52f3267c
Faiz Hidayatulloh <faizhid11@gmail.com>
2022-04-02 15:40:29 +0700

    make components for UI with existing html file to jsx file format

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import Arrived from "./components/Arrived";
+import AsideMenu from "./components/AsideMenu";
+import Browse from "./components/Browse";
+import Clients from "./components/Clients";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
 function App() {
-  return <h1 className="underline text-red-700">Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrived />
+      <Clients />
+      <AsideMenu />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
